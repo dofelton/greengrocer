497f28116ccbaa3c559bd6ca09e79f7abb9f9c8f
dofelton <90722967+dofelton@users.noreply.github.com>
2024-07-26 23:26:51 -0500

    Update Products.mjs

--- js/Products.mjs
+++ js/Products.mjs
@@ -2,7 +2,7 @@ import { renderListWithTemplate, getLocalStorage, setLocalStorage } from "./util
 
 export function productCard(product) {
     return `<li class="product-card" id="addToList">
-    <a href="../list" class="product-"anchor">
+    <a href="list.html" class="product-"anchor">
     <h3>${product.description}</h3>
     <p>${product.additionalDescriptions}</p>
     <p>${product.foodCategory}</p>
